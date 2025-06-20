59227e944a427a2038d2cfc940f172323f7da913
mongsil19 <git.yongmin@gmail.com>
2025-06-20 10:31:18 +0900

    feat: 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-// import Parent from './components/Parent'
-import { useCountStore } from './stores/count'
-import { useUserStore } from './stores/user'
+// // import Parent from './components/Parent'
+// import { useCountStore } from './stores/count'
+// import { useUserStore } from './stores/user'
+
+// const App = () => {
+//   // const count = useCountStore(state => state.count)
+//   // const increase = useCountStore(state => state.increase)
+//   // const double = useCountStore(state => state.double)
+//   // const user = useUserStore(state => state.user)
+//   // const setName = useUserStore(state => state.setName)
+//   // const setFirstEmail = useUserStore(state => state.setFirstEmail)
+//   // return (
+//     <>
+//       {/* <div>{user.name}</div>
+//       <h2 onClick={() => setName('min')}>setname</h2>
+//       <h3 onClick={() => setFirstEmail('min@daum.net')}>
+//         {user.address.emails[0]}
+//       </h3>
+//       <button onClick={increase}>{count}</button>
+//       <div>{double}</div> */}
+//     </>
+//   )
+// }
+// export default App
 
 const App = () => {
-  const count = useCountStore(state => state.count)
-  const increase = useCountStore(state => state.increase)
-  const double = useCountStore(state => state.double)
-  const user = useUserStore(state => state.user)
-  const setName = useUserStore(state => state.setName)
-  const setFirstEmail = useUserStore(state => state.setFirstEmail)
-  return (
-    <>
-      <div>{user.name}</div>
-      <h2 onClick={() => setName('min')}>setname</h2>
-      <h3 onClick={() => setFirstEmail('min@daum.net')}>
-        {user.address.emails[0]}
-      </h3>
-      <button onClick={increase}>{count}</button>
-      <div>{double}</div>
-    </>
-  )
+  return <div></div>
 }
+
 export default App
